e478c582ff6b9ee781ae6f4ad656bae875d9d001
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-01 14:15:27 +0300

    преобразование double в MyFirstProgram

--- sandbox/src/main/java/ru/learning/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/learning/sandbox/MyFirstProgram.java
@@ -5,6 +5,9 @@ public class MyFirstProgram {
     public static void main(String[] args) {
         hello("мир");
         hello("брат");
+        
+        System.out.printf("%.2f", 3.0005);
+        System.out.println();
 
         Square s = new Square(0.5);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
