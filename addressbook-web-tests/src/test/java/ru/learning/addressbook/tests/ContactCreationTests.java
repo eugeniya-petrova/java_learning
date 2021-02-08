07fb7f2ffe9f9495874a5464616bf2affdebc9a6
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-08 18:20:58 +0300

    ApplicationManager разделён на несколько классов-помощников

--- addressbook-web-tests/src/test/java/ru/learning/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/learning/addressbook/tests/ContactCreationTests.java
@@ -7,10 +7,10 @@ public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() throws Exception {
-    app.initContactCreation();
-    app.fillContactForm(new ContactData("Дмитрий", "Менделеев", "Тобольск, улица Тюменская, дом 5", "+7(495)1112233", "+79034445566", "+74957778899", "+74950001122", "test0802@test.ru", "test0802-1@test.ru", "test0802-2@test.ru", "Санкт-Петербург, улица Пушкина, дом 6"));
-    app.submitContactCreation();
-    app.returnToHomePage();
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Дмитрий", "Менделеев", "Тобольск, улица Тюменская, дом 5", "+7(495)1112233", "+79034445566", "+74957778899", "+74950001122", "test0802@test.ru", "test0802-1@test.ru", "test0802-2@test.ru", "Санкт-Петербург, улица Пушкина, дом 6"));
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().returnToHomePage();
   }
 
 }
