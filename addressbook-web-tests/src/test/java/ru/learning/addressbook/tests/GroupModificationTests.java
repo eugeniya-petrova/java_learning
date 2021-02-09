065488338d3ef0550719a91d1640f2c0d45e37ef
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-09 12:02:10 +0300

    новый класс GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/learning/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,18 @@
+package ru.learning.addressbook.tests;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import ru.learning.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testModificationGroup() {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData(By.name("group_name").toString() + "модиф", "Группа 9 февраля модиф", "Группа 9 февраля 2021 модиф"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
