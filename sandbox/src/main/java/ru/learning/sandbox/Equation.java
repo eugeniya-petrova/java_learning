84082e041c697b85f696adb8844ae08681d12816
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-12 19:07:05 +0300

    sandbox: форматирование разметки в классе Equation

--- sandbox/src/main/java/ru/learning/sandbox/Equation.java
+++ sandbox/src/main/java/ru/learning/sandbox/Equation.java
@@ -13,16 +13,14 @@ public class Equation {
         this.b = b;
         this.c = c;
 
-        double d = b*b - 4*a*c;
+        double d = b * b - 4 * a * c;
 
         if (d > 0) {
             n = 2;
+        } else if (d == 0) {
+            n = 1;
         } else {
-            if (d == 0) {
-                n = 1;
-            } else {
-                n = 0;
-            }
+            n = 0;
         }
     }
 
