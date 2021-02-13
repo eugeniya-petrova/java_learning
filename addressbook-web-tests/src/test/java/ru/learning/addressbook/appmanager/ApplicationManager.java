651b44b76bfc6f813252d9fc1750f6b80a0db9fa
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-13 18:50:54 +0300

    addressbook: в классе ApplicationManager добавлена зависимость implicitlyWait от выбора браузера

--- addressbook-web-tests/src/test/java/ru/learning/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/learning/addressbook/appmanager/ApplicationManager.java
@@ -3,9 +3,8 @@ package ru.learning.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
@@ -26,13 +25,12 @@ public class ApplicationManager {
     public void init() {
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.IE)) {
-            wd = new InternetExplorerDriver();
+            wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         }
 
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
