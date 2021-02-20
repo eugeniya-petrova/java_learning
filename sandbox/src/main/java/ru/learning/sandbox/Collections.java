d764b6f651e2ad8a01201ee68f32c0f9f4e58f1c
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-20 09:47:41 +0300

    sandbox: новый класс Collections

---
+++ sandbox/src/main/java/ru/learning/sandbox/Collections.java
@@ -0,0 +1,8 @@
+package ru.learning.sandbox;
+
+public class Collections {
+
+    public static void main(String[] args) {
+        String[] langs = {"English", "French", "Russian", "German"};
+    }
+}
