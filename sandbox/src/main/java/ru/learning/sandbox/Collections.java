f2df69c8850ceb6bf5cea85a4b49dcbe5bab5639
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-02-20 19:01:37 +0300

    sandbox: в классе Collections реализован метод для работы со списками

--- sandbox/src/main/java/ru/learning/sandbox/Collections.java
+++ sandbox/src/main/java/ru/learning/sandbox/Collections.java
@@ -1,8 +1,29 @@
 package ru.learning.sandbox;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collections {
 
     public static void main(String[] args) {
-        String[] langs = {"English", "French", "Russian", "German"};
+        String[] langs = {"английский", "французский", "русский", "немецкий"};
+
+        /*
+        List<String> languages = new ArrayList<String>();
+        languages.add("Java");
+        languages.add("Python");
+        languages.add(null);
+         */
+
+        List<String> languages = Arrays.asList("Java", "Python", null, "C#");
+
+        for (String l : languages) {
+            if (l != null) {
+                System.out.println("Я изучаю " + l);
+            } else {
+                System.out.println("Я ничего не изучаю");
+            }
+        }
     }
 }
