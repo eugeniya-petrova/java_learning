b1d8133ad74e162bbff29d735e9cb02c72ae3dcc
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-03-24 09:56:29 +0300

    mantis: форматирование вёрстки

--- mantis-tests/src/test/java/ru/learning/mantis/tests/ChangePasswordTests.java
+++ mantis-tests/src/test/java/ru/learning/mantis/tests/ChangePasswordTests.java
@@ -13,38 +13,38 @@ import javax.mail.MessagingException;
 
 import static org.testng.Assert.assertTrue;
 
-public class ChangePasswordTests extends TestBase{
-	
-	//перед каждым тестом запускаем почтовый сервер заново, чтобы не было взаимовлияния тестов друг на друга
-	@BeforeMethod
-	public void startMailServer() {
-		app.mail().start();
-	}
-	
-	@Test
-	public void testChangePassword() throws IOException, MessagingException{
-		UserData user = app.db().userSet().iterator().next();
-		app.changePassword().start(user);
-		List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000); //должно придти одно письмо, ожидание 10 секунд
-		String confirmationLink = app.mail().findConfirmationLink(mailMessages, user.getEmail());
-		
-		long now = System.currentTimeMillis();
-		String newPassword = String.format("pass%s", now);
-		app.changePassword().finish(confirmationLink, user, newPassword);
-		
-		HttpSession session = app.newSession();
-		assertTrue(session.preLogin(user.getUsername()));
+public class ChangePasswordTests extends TestBase {
+
+    //перед каждым тестом запускаем почтовый сервер заново, чтобы не было взаимовлияния тестов друг на друга
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
+    @Test
+    public void testChangePassword() throws IOException, MessagingException {
+        UserData user = app.db().userSet().iterator().next();
+        app.changePassword().start(user);
+        List<MailMessage> mailMessages = app.mail().waitForMail(1, 10000); //должно придти одно письмо, ожидание 10 секунд
+        String confirmationLink = app.mail().findConfirmationLink(mailMessages, user.getEmail());
+
+        long now = System.currentTimeMillis();
+        String newPassword = String.format("pass%s", now);
+        app.changePassword().finish(confirmationLink, user, newPassword);
+
+        HttpSession session = app.newSession();
+        assertTrue(session.preLogin(user.getUsername()));
         assertTrue(session.login(user.getUsername(), newPassword));
         assertTrue(session.isLoggedInAs(user.getUsername()));
-	}
-	
-	//после каждого теста останавливаем почтовый сервер
-	@AfterMethod(alwaysRun = true)
-	public void stopMailServer() {
-		app.mail().stop();
-	}
+    }
+
+    //после каждого теста останавливаем почтовый сервер
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
+    }
 
 
-	//String newPassword = app.db().userSet().stream().filter((u) -> ((Integer) u.getId()).equals(user.getId()))
-	//		.findAny().get().getPassword();
+    //String newPassword = app.db().userSet().stream().filter((u) -> u.getId() == user.getId())
+    //		.findAny().get().getPassword();
 }
--- mantis-tests/src/test/java/ru/learning/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/learning/mantis/tests/RegistrationTests.java
@@ -12,31 +12,31 @@ import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 
-public class RegistrationTests extends TestBase{
-	
-	@BeforeMethod
-	public void startMailServer() {
-		app.mail().start();
-	}
-	
-	@Test
-	public void testRegistration() throws IOException, MessagingException{
-		String user = "user1";
-		String password = "password";
-		String email = "user1@localhost.domain";
-		app.registration().start(user, email);
-		List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000); //должно придти два письма, ожидание 10 секунд
-		String confirmationLink = app.mail().findConfirmationLink(mailMessages, email);
-		app.registration().finish(confirmationLink, password);
-		
-		HttpSession session = app.newSession();
-		assertTrue(session.preLogin(user));
+public class RegistrationTests extends TestBase {
+
+    @BeforeMethod
+    public void startMailServer() {
+        app.mail().start();
+    }
+
+    @Test
+    public void testRegistration() throws IOException, MessagingException {
+        String user = "user1";
+        String password = "password";
+        String email = "user1@localhost.domain";
+        app.registration().start(user, email);
+        List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000); //должно придти два письма, ожидание 10 секунд
+        String confirmationLink = app.mail().findConfirmationLink(mailMessages, email);
+        app.registration().finish(confirmationLink, password);
+
+        HttpSession session = app.newSession();
+        assertTrue(session.preLogin(user));
         assertTrue(session.login(user, password));
         assertTrue(session.isLoggedInAs(user));
-	}
-	
-	@AfterMethod(alwaysRun = true)
-	public void stopMailServer() {
-		app.mail().stop();
-	}
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void stopMailServer() {
+        app.mail().stop();
+    }
 }
