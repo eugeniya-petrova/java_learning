cc7d4745782a645e75f6b2d8d0c2bdf28788c962
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-03-24 09:54:48 +0300

    mantis: изменено обращение к ApplicationManager из классов-помощников

--- mantis-tests/src/test/java/ru/learning/mantis/appmanager/RegistrationHelper.java
+++ mantis-tests/src/test/java/ru/learning/mantis/appmanager/RegistrationHelper.java
@@ -1,28 +1,24 @@
 package ru.learning.mantis.appmanager;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper extends HelperBase{
-	
-	private WebDriver wd;
-	
-	public RegistrationHelper(ApplicationManager app) {
-		super(app);
-		wd = app.getDriver();
-	}
+public class RegistrationHelper extends HelperBase {
+
+    public RegistrationHelper(ApplicationManager app) {
+        super(app);
+    }
 
     public void start(String username, String email) {
-		wd.get(app.getProperty("web.baseURL") + "signup_page.php");
-		type(By.name("username"), username);
-		type(By.name("email"), email);
-		click(By.cssSelector("input[type='submit']"));
-	}
-	
-	public void finish(String confirmationLink, String password) {
-		wd.get(confirmationLink);
-		type(By.name("password"), password);
-		type(By.name("password_confirm"), password);
-		click(By.cssSelector("input[type='submit']"));
-	}
+        wd.get(app.getProperty("web.baseURL") + "signup_page.php");
+        type(By.name("username"), username);
+        type(By.name("email"), email);
+        click(By.cssSelector("input[type='submit']"));
+    }
+
+    public void finish(String confirmationLink, String password) {
+        wd.get(confirmationLink);
+        type(By.name("password"), password);
+        type(By.name("password_confirm"), password);
+        click(By.cssSelector("input[type='submit']"));
+    }
 }
