06693165d846ac2f6922c0487b15b756fa3b4ed9
eugeniya-petrova <eugeniya.petrova@gmail.com>
2021-06-07 18:31:08 +0300

    HM_addressbook: реализованы классы GroupStepDefinitions и GroupTests для Cucumber

--- addressbook-web-tests/src/test/java/ru/learning/addressbook/bdd/GroupStepDefinitions.java
+++ addressbook-web-tests/src/test/java/ru/learning/addressbook/bdd/GroupStepDefinitions.java
@@ -1,4 +1,52 @@
 package ru.learning.addressbook.bdd;
 
+import java.io.IOException;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import ru.learning.addressbook.appmanager.ApplicationManager;
+import ru.learning.addressbook.model.GroupData;
+import ru.learning.addressbook.model.GroupSet;
+import org.openqa.selenium.remote.BrowserType;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class GroupStepDefinitions {
+	
+	private ApplicationManager app;
+	private GroupSet groupSet;
+	private GroupData newGroup;
+	
+	@Before //аннотация для Cucumber
+	public void init() throws IOException {
+		app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+		app.init();
+	}
+	
+	@After //аннотация для Cucumber
+	public void stop() {
+		app.stop();
+		app = null;
+	}
+	
+	@Given("^a set of groups$") //проверяется точное соответствие текста
+	public void loadGroups() {
+		groupSet = app.db().groupSet();
+	}
+	
+	@When("^I create a new group with name (.+), header (.+) and footer (.+)$") //(.+) - входные параметры метода
+	public void createGroup(String name, String header, String footer) {
+		newGroup = new GroupData().withName(name).withHeader(header).withFooter(footer);
+		app.group().create(newGroup);
+	}
+	
+	@Then("^the new set of groups is equal to the old set with added group$")
+	public void verifyGroupCreation() {
+		GroupSet newGroupSet = app.db().groupSet();
+		assertThat(newGroupSet, equalTo(groupSet.withAdded(newGroup)));
+	}
 }
--- addressbook-web-tests/src/test/java/ru/learning/addressbook/bdd/GroupTests.java
+++ addressbook-web-tests/src/test/java/ru/learning/addressbook/bdd/GroupTests.java
@@ -1,5 +1,11 @@
 package ru.learning.addressbook.bdd;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
 //класс-запускатель для сценариев
-public class GroupTests {
+@CucumberOptions(features = "classpath:bdd", plugin = {"pretty", "html:build/cucumber-report"})
+//features - где именно находятся файлы с описанием сценариев
+//plugin - формат вывода на консоль, директория отчёта
+public class GroupTests extends AbstractTestNGCucumberTests {
 }
